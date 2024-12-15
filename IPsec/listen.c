60d65452d527e25b33767dda130cee5446be025e
Andriy <andrey.lototskiy@gmail.com>
2024-12-15 14:40:42 -0700

    Implemented secure transmission protocol

--- IPsec/listen.c
+++ IPsec/listen.c
@@ -17,8 +17,16 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
+#include <math.h>
 
 #define BUFSIZE 1024
+
+
+#define PRIME_MODULUS 23
+#define GENERATOR 5
+
+void bitWiseXOR(char *buf, unsigned char shared_key);
 
 /*
  * error - wrapper for perror
@@ -29,91 +37,130 @@ void error(char *msg) {
 }
 
 int main(int argc, char **argv) {
-  int sockfd; /* socket */
-  int portno; /* port to listen on */
-  int clientlen; /* byte size of client's address */
-  struct sockaddr_in serveraddr; /* server's addr */
-  struct sockaddr_in clientaddr; /* client addr */
-  struct hostent *hostp; /* client host info */
-  char buf[BUFSIZE]; /* message buf */
-  char *hostaddrp; /* dotted decimal host addr string */
-  int optval; /* flag value for setsockopt */
-  int n; /* message byte size */
-
-  /* 
-   * check command line arguments 
-   */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
-  portno = atoi(argv[1]);
-
-  /* 
-   * socket: create the parent socket 
-   */
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sockfd < 0) {
-    error("ERROR opening socket");
-  }
-
-  /* setsockopt: Handy debugging trick that lets 
-   * us rerun the server immediately after we kill it; 
-   * otherwise we have to wait about 20 secs. 
-   * Eliminates "ERROR on binding: Address already in use" error. 
-   */
-  optval = 1;
-  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
-	     (const void *)&optval , sizeof(int));
-
-  /*
-   * build the server's Internet address
-   */
-  bzero((char *) &serveraddr, sizeof(serveraddr));
-  serveraddr.sin_family = AF_INET;
-  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serveraddr.sin_port = htons((unsigned short)portno);
-
-  /* 
-   * bind: associate the parent socket with a port 
-   */
-  if (bind(sockfd, (struct sockaddr *) &serveraddr, 
-	   sizeof(serveraddr)) < 0) 
-    error("ERROR on binding");
-
-  /* 
-   * main loop: wait for a datagram, then echo it
-   */
-  clientlen = sizeof(clientaddr);
-  while (1) {
-
-    /*
-     * recvfrom: receive a UDP datagram from a client
-     */
-    bzero(buf, BUFSIZE);
-    n = recvfrom(sockfd, buf, BUFSIZE, 0,(struct sockaddr *) &clientaddr, (socklen_t *)&clientlen);
-    if (n < 0)
-      error("ERROR in recvfrom");
+    int sockfd; /* socket */
+    int portno; /* port to listen on */
+    int clientlen; /* byte size of client's address */
+    struct sockaddr_in serveraddr; /* server's addr */
+    struct sockaddr_in clientaddr; /* client addr */
+    struct hostent *hostp; /* client host info */
+    char buf[BUFSIZE]; /* message buf */
+    char *hostaddrp; /* dotted decimal host addr string */
+    int optval; /* flag value for setsockopt */
+    int n; /* message byte size */
 
     /* 
-     * gethostbyaddr: determine who sent the datagram
-     */
-    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-    if (hostp == NULL)
-    {
-        error("ERROR on gethostbyaddr");
+    * check command line arguments 
+    */
+    if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
     }
-      
-    hostaddrp = inet_ntoa(clientaddr.sin_addr);
+    portno = atoi(argv[1]);
 
-
-    if (hostaddrp == NULL)
-    {
-        error("ERROR on inet_ntoa\n");
+    /* 
+    * socket: create the parent socket 
+    */
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+    error("ERROR opening socket");
     }
 
-    printf("server received message from %s (%s)\n", hostp->h_name, hostaddrp);
-    printf("server received %d/%d bytes: %s\n", (int)strlen(buf), n, buf);
+    /* setsockopt: Handy debugging trick that lets 
+    * us rerun the server immediately after we kill it; 
+    * otherwise we have to wait about 20 secs. 
+    * Eliminates "ERROR on binding: Address already in use" error. 
+    */
+    optval = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
+            (const void *)&optval , sizeof(int));
+
+    /*
+    * build the server's Internet address
+    */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)portno);
+
+    /* 
+    * bind: associate the parent socket with a port 
+    */
+    if (bind(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) 
+    {
+        error("ERROR on binding");
+    }
     
-  }
+
+    clientlen = sizeof(clientaddr);
+
+    // Again, before receiving inputs, generate our private, and public key.
+
+
+    srand(time(NULL));
+    unsigned long private_key = rand() % 13;
+
+    unsigned long public_key = (long)pow((double)GENERATOR, (double)private_key) % PRIME_MODULUS;
+    printf("Prime Modulus is: %d\nGenerator is:%d\n", PRIME_MODULUS, GENERATOR);
+    printf("Private key is:%lu\nPublic key is: %lu\n", private_key, public_key);
+    printf("%ld\n", (long)pow((double)GENERATOR, (double)private_key));
+
+    // expect to receive a public key
+    bzero(buf, BUFSIZE);
+    n = recvfrom(sockfd, buf, BUFSIZE, 0,(struct sockaddr *) &clientaddr, (socklen_t *)&clientlen);
+    if(n < 0)
+    {
+        error("ERROR in recvfom");
+    }
+
+    unsigned long client_public_key = strtoul(buf, NULL, 10);
+
+    unsigned char shared_key = (long)pow((double)client_public_key, (double)private_key) % PRIME_MODULUS;
+
+    // Finally, send the public key back to the client
+    bzero(buf, BUFSIZE);
+    snprintf(buf, BUFSIZE, "%lu", public_key);
+    n = sendto(sockfd, buf, BUFSIZE, 0, (struct sockaddr *)&clientaddr, clientlen);
+    if(n < 0)
+    {
+        error("ERROR in sendto");
+    }
+
+    printf("Received a key of: %lu\n", client_public_key);
+    printf("Shared key is %d\n", shared_key);
+
+    while (1) {
+        bzero(buf, BUFSIZE);
+        n = recvfrom(sockfd, buf, BUFSIZE, 0,(struct sockaddr *) &clientaddr, (socklen_t *)&clientlen);
+        if (n < 0)
+            error("ERROR in recvfrom");
+
+
+        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        if (hostp == NULL)
+        {
+            error("ERROR on gethostbyaddr");
+        }
+            
+        hostaddrp = inet_ntoa(clientaddr.sin_addr);
+
+        if (hostaddrp == NULL)
+        {
+            error("ERROR on inet_ntoa\n");
+        }
+
+        printf("Received message from %s (%s)\n", hostp->h_name, hostaddrp);
+        printf("Received %d/%d bytes: %s\n", (int)strlen(buf), n, buf);
+
+        // Finally, use the shared key to decrypt our message
+        bitWiseXOR(buf, shared_key);
+        printf("Decrypted Message: %s", buf);
+    }
+}
+
+void bitWiseXOR(char *buf, unsigned char shared_key)
+{
+    for(int i = 0; i < BUFSIZE-1; i++)
+    {
+        buf[i] = buf[i] ^ shared_key;
+    }
 }
--- IPsec/talk.c
+++ IPsec/talk.c
@@ -10,8 +10,17 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <math.h>
+#include <time.h>
 
 #define BUFSIZE 1024
+
+
+#define PRIME_MODULUS 23
+#define GENERATOR 5
+
+
+void bitWiseXOR(char *buf, unsigned char shared_key);
 
 /* 
  * error - wrapper for perror
@@ -40,11 +49,15 @@ int main(int argc, char **argv) {
     /* socket: create the socket */
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) 
+    {
         error("ERROR opening socket");
+    }
+        
 
     /* gethostbyname: get the server's DNS entry */
     server = gethostbyname(hostname);
-    if (server == NULL) {
+    if (server == NULL) 
+    {
         fprintf(stderr,"ERROR, no such host as %s\n", hostname);
         exit(0);
     }
@@ -54,8 +67,44 @@ int main(int argc, char **argv) {
     serveraddr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(portno);
-
+    serverlen = sizeof(serveraddr);
     
+    // Before we send any messages, we need to generate a public and private key, and send the public key to the server.
+    //  Generate the private key as some random number
+    srand(time(NULL));
+    unsigned long private_key = rand() % 13;
+
+    unsigned long public_key = (long)pow((double)GENERATOR, (double)private_key) % PRIME_MODULUS;
+    printf("Prime Modulus is: %d\nGenerator is:%d\n", PRIME_MODULUS, GENERATOR);
+    printf("Private key is:%lu\nPublic key is: %lu\n", private_key, public_key);
+
+    // Next, send the public key to the server.
+    // Convert the public key to a string
+    bzero(buf, BUFSIZE);
+    snprintf(buf, BUFSIZE, "%lu", public_key);
+    n = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *)&serveraddr, serverlen);
+    if(n < 0)
+    {
+        error("ERROR in sendto");
+    }
+
+    // Next, await the response for the server's public key
+    unsigned long server_public_key;
+    bzero(buf, BUFSIZE);
+    n = recvfrom(sockfd, buf, BUFSIZE, 0, (struct sockaddr *)&serveraddr, (socklen_t *)&serverlen); 
+    if(n < 0)
+    {
+        error("ERROR in recvfrom");
+    }
+    server_public_key = strtoul(buf, NULL, 10);
+
+    //Finally, generate the shared key
+
+    unsigned char shared_key = (long)pow((double)server_public_key, (double)private_key) % PRIME_MODULUS;
+
+    printf("Received a key of: %lu\n", server_public_key);
+    printf("Shared key is %d\n", shared_key);
+
     // Send as many messages as we want to the server
     while(1)
     {
@@ -63,8 +112,11 @@ int main(int argc, char **argv) {
         printf("Please enter msg: ");
         fgets(buf, BUFSIZE, stdin);
 
+        // Before sending the message, XOR eveyr char with our shared key
+
+        bitWiseXOR(buf, shared_key);
+
         /* send the message to the server */
-        serverlen = sizeof(serveraddr);
         n = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *)&serveraddr, serverlen);
         if (n < 0) 
         error("ERROR in sendto");
@@ -74,4 +126,13 @@ int main(int argc, char **argv) {
 
     
     return 0;
+}
+
+
+void bitWiseXOR(char *buf, unsigned char shared_key)
+{
+    for(int i = 0; i < BUFSIZE; i++)
+    {
+        buf[i] = buf[i] ^ shared_key;
+    }
 }
